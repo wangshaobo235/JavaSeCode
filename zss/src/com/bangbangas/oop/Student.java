5ee7c15ebde200172205f24891329a8d8eb8950d
wangshaobo235 <15732030967@163.com>
2023-11-01 22:12:51 +0800

    提交代码

--- zss/src/com/bangbangas/oop/Student.java
+++ zss/src/com/bangbangas/oop/Student.java
@@ -1,4 +1,33 @@
 package com.bangbangas.oop;
 
 public class Student {
+    int id;
+    static int score;
+    /*
+
+     */
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+
+        Student.score = score;
+    }
 }
--- zss/src/com/bangbangas/oop/StudentTest.java
+++ zss/src/com/bangbangas/oop/StudentTest.java
@@ -1,4 +1,26 @@
 package com.bangbangas.oop;
 
 public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setId(10);
+        student.setScore(20);
+        Student student1 = new Student();
+        student1.setId(20);
+        student1.setScore(30);
+        Student student2 = new Student();
+        student2.setId(30);
+        student2.setScore(40);
+
+        System.out.println(student.getScore());
+        System.out.println(student1.getScore());
+        System.out.println(student2.getScore());
+        test();
+
+
+    }
+
+    public static void test(){
+        System.out.println("");
+    }
 }
