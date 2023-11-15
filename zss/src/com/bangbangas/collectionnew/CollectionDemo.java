eb8d8d46eda13c8854b7b57bb2382cbf7c04ba5a
wangshaobo235 <15732030967@163.com>
2023-11-15 09:05:48 +0800

    集合map测试

--- zss/src/com/bangbangas/collectionnew/CollectionDemo.java
+++ zss/src/com/bangbangas/collectionnew/CollectionDemo.java
@@ -1,43 +1,5 @@
 package com.bangbangas.collectionnew;
 
-<<<<<<< HEAD
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.ListIterator;
-
-/**
- * ClassName: CollectionDemo
- * Package: com.bangbangas.collectionnew
- * Description:
- *
- * @Author 王少波
- * @Create 2023/11/14 16:48
- * @Version 1.0
- */
-public class CollectionDemo {
-    public static void main(String[] args) {
-        /*
-        collection list set map
-         */
-        ArrayList<String> arrayList = new ArrayList();
-        arrayList.add("a");
-        arrayList.add("b");
-        arrayList.add("c");
-        for (String s : arrayList) {
-            System.out.println(s);
-        }
-        Iterator<String> iterator = arrayList.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
-
-        for (int i = 0; i < arrayList.size(); i++) {
-            System.out.println(arrayList.get(i));
-        }
-
-=======
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -138,6 +100,6 @@ class Book{
                 ", author='" + author + '\'' +
                 ", price=" + price +
                 '}';
->>>>>>> 0345d0e52878aa9702d03268b75044371a4fd923
+
     }
 }
